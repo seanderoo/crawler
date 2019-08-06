f75b917731077076326917efcf190a7e0c4b5875
Seán de Roo <sean.de.roo@hva.nl>
2019-08-06 20:13:03 +0200

    leest mijn files niet

--- src/main/java/model/WebSpider.java
+++ src/main/java/model/WebSpider.java
@@ -3,103 +3,125 @@ package model;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-
+import java.io.*;
 import java.util.ArrayList;
-import java.util.List;
-
+import java.util.Scanner;
 
 public class WebSpider {
 
     public static void main(String[] args) {
 
-        String url1 = "https://www.bmc.nl/";
-        String url2 = "https://www.randstad.nl/";
-        String url3 = "https://fcbayern.com/en";
-        String url4 = "https://www.unive.nl/";
-        String url5 = "https://www.ns.nl/en";
-        String url6 = "https://www.weleda.nl/";
-        String url7 = "https://www.anwb.nl/";
-        String url8 = "https://www.greetz.nl";
-        String url9 = "https://www.ovoenergy.com";
-        String url10 = "https://www.airmiles.nl/";
-        String url11 = "https://ingenico.nl/en";
-        String url12 = "https://www.hellermanntyton.nl/";
-        String url13 = "https://www.forever21.com/eu/shop";
-        String url14 = "https://www.dpv.de/";
-        String url15 = "https://www.triodos.com/";
-
-        List<String> urls = new ArrayList<>();
-
-        int amount = 0;
-        int amountWebfiles = 0;
-        int amountBinaries = 0;
-        int amountBloomreach = 0;
-
-        urls.add(url1);
-        urls.add(url2);
-        urls.add(url3);
-        urls.add(url4);
-        urls.add(url5);
-        urls.add(url6);
-        urls.add(url7);
-        urls.add(url8);
-        urls.add(url9);
-        urls.add(url10);
-        urls.add(url11);
-        urls.add(url12);
-        urls.add(url13);
-        urls.add(url14);
-        urls.add(url15);
+        ArrayList<String> urls = cvsToArrayList("bloomreach_bedrijven.csv");
 
         try {
-            for (String url : urls) {
+
+            System.out.println("Checking " + urls.size() + " URL's...\n");
+
+            for (String url: urls) {
+
+                System.out.println("URL: " + url +"\n");
                 Document document = Jsoup.connect(url).get();
 
-                Elements linkWebfiles = document.select("*[:contains(webfiles)]");
-                Elements useWebfiles = document.select("*[xlink:href*=webfiles]");
-                Elements metaWebfiles = document.select("*[content*=webfiles]");
-                Elements scriptWebfiles = document.select("*[src*=webfiles]");
-                Elements imageWebfiles = document.select("*[xlink:href*=webfiles]");
-
-                Elements imgBinaries = document.select("*[src*=binaries]");
-                Elements sourceBinaries = document.select("*[srcset*=binaries]");
-                Elements spanBinariesLarge = document.select("*[data-image-large*=binaries]");
-                Elements spanBinariesMedium = document.select("*[data-image-medium*=binaries]");
-                Elements spanBinariesSmall = document.select("*[data-image-small*=binaries]");
-                Elements metaBinaries = document.select("*[content*=binaries]");
-                Elements divBinaries = document.select("*[style*=binaries]");
-                Elements imgDataOriginal = document.select("*[data-original*=binaries]");
-                Elements imgDataSourceBinaries = document.select("*[data-src*=binaries]");
-
-                Elements bloomreachScript = document.select("script[src*=bloomreach]");
-
-
-                amountWebfiles = linkWebfiles.size() + useWebfiles.size() + metaWebfiles.size() + scriptWebfiles.size() +
-                        imageWebfiles.size();
-
-                amountBinaries = imgBinaries.size() + sourceBinaries.size() + spanBinariesLarge.size() + spanBinariesMedium.size() +
-                        spanBinariesSmall.size() +
-                        metaBinaries.size() + divBinaries.size() + imgDataOriginal.size() +
-                        imgDataSourceBinaries.size();
-
-                amountBloomreach = bloomreachScript.size();
-
-                amount = amountWebfiles + amountBinaries + amountBloomreach;
-
                 System.out.println(url);
-                System.out.println("hits: " + amount);
-                System.out.println("amount webfiles: " + amountWebfiles);
-                System.out.println("amount binaries: " + amountBinaries);
-                System.out.println("amount bloomreach: " + amountBloomreach);
+                System.out.println("Total Hits: " + checkHits(document));
+                System.out.println("Hits Webfiles: " + checkWebfiles(document));
+                System.out.println("Hits Binaries: " + checkBinaries(document));
+                System.out.println("Hits Bloomreach: " + checkBloomreach(document));
                 System.out.println();
-
-
             }
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Document getPageSource(String url) throws IOException {
+        Document document = Jsoup.connect(url).get();
+        return document;
+    }
+
+    private static int checkHits(Document document) {
+        return checkWebfiles(document) + checkBinaries(document) + checkBloomreach(document);
+    }
+
+    private static int checkBloomreach(Document document) {
+        int amountBloomreach;
+
+        Elements srcBloomReach = document.select("*[src*=bloomreach]");
+        Elements classBloomReach = document.select("*[class*=bloomreach]");
+
+        amountBloomreach = srcBloomReach.size() + classBloomReach.size();
+
+        return amountBloomreach;
+    }
+
+    private static int checkBinaries(Document document) {
+        int amountBinaries;
+
+        Elements srcBinaries = document.select("*[src*=binaries]");
+        Elements srcsetBinaries = document.select("*[srcset*=binaries]");
+        Elements dataImageLargeBinaries = document.select("*[data-image-large*=binaries]");
+        Elements dataImageMediumBinaries = document.select("*[data-image-medium*=binaries]");
+        Elements dataImageSmallBinaries = document.select("*[data-image-small*=binaries]");
+        Elements contentBinaries = document.select("*[content*=binaries]");
+        Elements styleBinaries = document.select("*[style*=binaries]");
+        Elements dataOriginalBinaries = document.select("*[data-original*=binaries]");
+        Elements dataSourceBinaries = document.select("*[data-src*=binaries]");
+
+        amountBinaries = srcBinaries.size() + srcsetBinaries.size() + dataImageLargeBinaries.size() + dataImageMediumBinaries.size() +
+                dataImageSmallBinaries.size() + contentBinaries.size() + styleBinaries.size() + dataOriginalBinaries.size() +
+                dataSourceBinaries.size();
+
+        return amountBinaries;
+    }
+
+    private static int checkWebfiles(Document document) {
+        int amountWebfiles;
+
+        Elements hrefWebfiles = document.select("*[href*=webfiles]");
+        Elements xlinkWebfiles = document.select("*[xlink:href*=webfiles]");
+        Elements contentWebfiles = document.select("*[content*=webfiles]");
+        Elements srcWebfiles = document.select("*[src*=webfiles]");
+        Elements dataBgWebfiles = document.select("*[data-bg*=webfiles]");
+        Elements dataSrcWebfiles = document.select("*[data-src*=webfiles]");
+
+        amountWebfiles = hrefWebfiles.size() + xlinkWebfiles.size() + contentWebfiles.size() + srcWebfiles.size()
+                + dataBgWebfiles.size() + dataSrcWebfiles.size();
+
+        return amountWebfiles;
+    }
+
+    private static ArrayList<String> cvsToArrayList(String csvFile) {
+        ArrayList<String> urlList = new ArrayList<>();
+        try {
+            File filename = new File(csvFile);
+            Scanner leesFile = new Scanner(filename);
+            while (leesFile.hasNext()) {
+                urlList.add(leesFile.next());
+            }
+        } catch (FileNotFoundException fout) {
+            fout.printStackTrace();
+        }
+        return urlList;
+    }
+
+    private static ArrayList<String> txtToArrayList() {
+        ArrayList<String> urlList = new ArrayList<>();
+
+        try {
+            File file = new File("websites.txt");
+            Scanner input = new Scanner(file);
+            while (input.hasNext()) {
+                urlList.add(input.next());
+            }
+            input.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
+        return urlList;
     }
+
+
 
 }
